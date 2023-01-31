4e2b08f5f213da8beb375f22d371956f3c991b66
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-01-31 13:34:27 +0530

    [Jyoti Rao] Create a Contacts in Address Book

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            List<Contacts> Person = new List<Contacts>();
             Console.WriteLine("*****----Welcome to Address Book Program----*****");
             Console.ReadLine();
         }
